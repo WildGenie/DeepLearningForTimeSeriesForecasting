641ae8ff19c414741e391b3c6b75d61620e9cb41
Sourcery AI <>
2022-12-01 01:52:43 +0000

    'Refactored by Sourcery'

--- common/progress_bar.py
+++ common/progress_bar.py
@@ -10,11 +10,8 @@ def update_progress(progress):
         progress = float(progress)
     if not isinstance(progress, float):
         progress = 0
-    if progress < 0:
-        progress = 0
-    if progress >= 1:
-        progress = 1
-
+    progress = max(progress, 0)
+    progress = min(progress, 1)
     block = int(round(bar_length * progress))
 
     clear_output(wait=True)
